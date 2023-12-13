1204e034fb9de7ec5ee0dc2153ce90af1a19feea
Julien Guillen <723506@student.dusd.net>
2023-12-13 15:27:45 -0800

    Added roof and Animations
    12/13/23

--- FlappyBirdJLNGuiilen/Assets/Scripts/Bird.cs
+++ FlappyBirdJLNGuiilen/Assets/Scripts/Bird.cs
@@ -6,12 +6,13 @@ public class Bird : MonoBehaviour
 {
     bool isDead = false;
     Rigidbody2D rb2d;
-
+    Animator anim;
     public float upForce = 200f;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,11 +24,12 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Steer");
             }
         }
         else if (isDead == true)
         {
-
+            anim.SetTrigger("Crash");
         }
     }
 
